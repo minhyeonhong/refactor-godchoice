da410ba70c32b115cfd39e8d8526bdfdc7f4bc03
Minhyeonhong <mhh2029@gmail.com>
2022-11-11 21:12:50 +0900

    지윤님 수정

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,12 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { __kakaoLogin } from "../modules/memberSlice";
 
 import postSlice from "../modules/postSlice";
+import memberSlice from "../modules/memberSlice"
 
 const store = configureStore({
     reducer: {
-        __kakaoLogin,
-        postSlice
+        postSlice,
+        memberSlice
     },
     //배포 모드일때 리덕스 데브툴 사용 안함
     devTools: process.env.REACT_APP_MOD !== 'production'
