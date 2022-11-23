744e82535a51c2a9a26952a85146596fec54a321
Minhyeonhong <mhh2029@gmail.com>
2022-11-23 20:53:58 +0900

    인스턴스 수정

--- src/api/api-functions/commentApis.js
+++ src/api/api-functions/commentApis.js
@@ -1,11 +1,11 @@
-import { hInstance, nhInstance } from "../instance";
+import { instance } from "../instance";
 
 export const commentApis = {
     /**댓글 조회 */
-    getCommentAX: (obj) => hInstance.get(`${process.env.REACT_APP_API_URL}/comments/${obj.postId}/${obj.kind}`),
+    getCommentAX: (obj) => instance.get(`${process.env.REACT_APP_API_URL}/comments/${obj.postId}/${obj.kind}`),
     /**댓글 등록 */
-    insertCommentAX: (obj) => hInstance.post(`${process.env.REACT_APP_API_URL}/comments/${obj.postId}/${obj.kind}`, obj.comment),
+    insertCommentAX: (obj) => instance.post(`${process.env.REACT_APP_API_URL}/comments/${obj.postId}/${obj.kind}`, obj.comment),
     /**댓글 삭제 */
-    deleteCommentAX: (obj) => hInstance.delete(`${process.env.REACT_APP_API_URL}/comments/${obj.postId}/${obj.commentId}/${obj.kind}`),
+    deleteCommentAX: (obj) => instance.delete(`${process.env.REACT_APP_API_URL}/comments/${obj.postId}/${obj.commentId}/${obj.kind}`),
 
 }
