cf0740d962803091ac8ef9e532be4392fa4d3d1e
Minhyeonhong <mhh2029@gmail.com>
2023-06-11 15:31:55 +0900

    [#23]Fix : useInfiniteQuery를 사용하여 무한스크롤 구현
    queryFn의 매개변수가 구조분해 할당으로 받고있어 정해진 변수명을 사용했어야 했습니다.
    기존 변수명 startAfterSnapshot
    명시된 변수명 pageParam

--- src/components/home/List.jsx
+++ src/components/home/List.jsx
@@ -10,10 +10,7 @@ import { useNavigate } from 'react-router-dom';
 import { BookmarkFill } from "../../assets/index";
 import PageState from '../common/PageState';
 import { useInfiniteQuery } from '@tanstack/react-query';
-import { postApis } from '../../api/api-functions/postApis';
 import { getPosts } from '../../firestore/module/post';
-import { useCallback } from 'react';
-import { useState } from 'react';
 
 const List = ({ searchState }) => {
 
@@ -21,114 +18,62 @@ const List = ({ searchState }) => {
     const [ref, inView] = useInView();
 
     //리스트 받아오기
-    // const result = useInfiniteQuery({
-    //     queryKey: ['postList'],
-    //     queryFn: ({ startAfterSnapshot }) => getPosts(searchState, startAfterSnapshot),
-    //     getNextPageParam: ({ isLastPage, lastSnapshot }) => {
-    //         if (!isLastPage) return lastSnapshot;
-    //     },
-    //     refetchOnWindowFocus: false,
-    // })
-
-    const [posts, setPosts] = useState();
-
-    const getFirst = useCallback(async () => {
-        try {
-            const res = await getPosts(searchState, {});
-
-            setPosts(res);
-        } catch (error) {
-            console.log("first error", error);
-        }
-    }, [getPosts]);
-
-    const getNext = useCallback(async (param) => {
-        try {
-            const res = await getPosts(searchState, param);
-
-            const obj = {
-                datas: [...posts.datas, ...res.datas],
-                isLastPage: res.isLastPage,
-                lastSnapshot: res.lastSnapshot
-            }
-
-            setPosts(obj);
-        } catch (error) {
-            console.log("next error", error);
-        }
-    }, [posts?.lastSnapshot]);
+    const result = useInfiniteQuery({
+        queryKey: ['postList'],
+        queryFn: ({ pageParam }) => getPosts(searchState, pageParam),
+        getNextPageParam: ({ isLastPage, lastSnapshot }) => {
+            if (!isLastPage) return lastSnapshot;
+        },
+        refetchOnWindowFocus: false,
+    })
 
     useEffect(() => {
-        getFirst();
-    }, [])
-
-    useEffect(() => {
-        console.log("posts", posts);
-    }, [posts])
-
-
-    useEffect(() => {
-        if (posts === undefined) return;
-        if (inView && !posts.isLastPage) {
-            getNext(posts.lastSnapshot);
+        // 사용자가 마지막 요소를 보고 있고, 로딩 중이 아니고 다음페이지가 있다면
+        if (inView && !result.isFetching && result.hasNextPage) {
+            result.fetchNextPage();
         }
-    }, [inView, posts])
+    }, [inView, result.isFetching])
 
-    // useEffect(() => {
-    //     // 사용자가 마지막 요소를 보고 있고, 로딩 중이 아니고 다음페이지가 있다면
-    //     if (inView && !result.isFetching && result.hasNextPage) {
-    //         //console.log("nextFetch");
-    //         result.fetchNextPage();
-    //     }
-    // }, [inView, result.isFetching])
-
-    // useEffect(() => {
-    //     //검색상태가 바뀌면 server state refetch
-    //     if (!result.isFetching) result.refetch(searchState, 0);
-    // }, [searchState])
+    useEffect(() => {
+        //검색상태가 바뀌면 server state refetch
+        if (!result.isFetching) result.refetch(searchState, 0);
+    }, [searchState])
 
     return (
         <StCardWrap>
-            {/* <PageState display={result.data?.pages[0].datas.length === 0 ? 'flex' : 'none'} state='notFound' imgWidth='25%' height='60vh'
-                text='리스트가 존재하지 않습니다.' /> */}
-            <PageState display={posts?.datas.length === 0 ? 'flex' : 'none'} state='notFound' imgWidth='25%' height='60vh'
+            <PageState display={result.data?.pages[0].datas.length === 0 ? 'flex' : 'none'} state='notFound' imgWidth='25%' height='60vh'
                 text='리스트가 존재하지 않습니다.' />
-            {/* {result.data?.pages.map((page, i) => (
-                <Fragment key={i}> */}
-            {posts?.datas.map((post) => (
-                <StCardItem key={post.postID} onClick={() => { navigate(`/event/${post.postID}`) }}>
-                    <StImgBox imgUrl={post.photoURIs[0]} >
-                        {post.bookMarkStatus &&
-                            <BookmarkFill style={{ margin: '4px 0 0 4px' }} />
-                        }
-                    </StImgBox>
-                    <StContentBox>
-                        <div className='titleBox'>{post.title.length > 10 ? post.title.substring(0, 9) + '...' : post.title}</div>
-                        <div>{post.category}</div>
-                        <div className='contentBox'>{post.content}</div>
-                        <div className='dtateBox'>
-                            <div>{post.endPeriod}</div>
-                            <div className='lookBox'>{post.viewCount}&nbsp;<BsEye style={{ width: '16px', height: '16px', marginTop: '2px' }} /></div>
-                        </div>
-                    </StContentBox>
-                </StCardItem>
+            {result.data?.pages.map((page, i) => (
+                <Fragment key={i}>
+                    {page.datas.map((post) => (
+                        <StCardItem key={post.postID} onClick={() => { navigate(`/event/${post.postID}`) }}>
+                            <StImgBox imgUrl={post.photoURIs[0]} >
+                                {post.bookMarkStatus &&
+                                    <BookmarkFill style={{ margin: '4px 0 0 4px' }} />
+                                }
+                            </StImgBox>
+                            <StContentBox>
+                                <div className='titleBox'>{post.title.length > 10 ? post.title.substring(0, 9) + '...' : post.title}</div>
+                                <div>{post.category}</div>
+                                <div className='contentBox'>{post.content}</div>
+                                <div className='dtateBox'>
+                                    <div>{post.endPeriod}</div>
+                                    <div className='lookBox'>{post.viewCount}&nbsp;<BsEye style={{ width: '16px', height: '16px', marginTop: '2px' }} /></div>
+                                </div>
+                            </StContentBox>
+                        </StCardItem>
+                    ))}
+                </Fragment>
             ))}
-            {/* </Fragment>
-            ))} */}
-            {/* <PageState
+            <PageState
                 display={result.isFetching ? 'flex' : 'none'}
                 state='notFound'
                 imgWidth='25%'
                 height=''
                 flexDirection='row'
-                text='검색중...' /> */}
-
-            {/* {
-                result.hasNextPage && <div ref={ref} />
-            } */}
-
+                text='검색중...' />
             {
-                !posts?.isLastPage && <div ref={ref} />
+                result.hasNextPage && <div ref={ref} />
             }
         </StCardWrap>
     );
