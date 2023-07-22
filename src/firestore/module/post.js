135bc0feb763ac3364e9b7bba688cacedc2a12ec
Minhyeonhong <mhh2029@gmail.com>
2023-07-22 14:20:31 +0900

    Fix : postPart제거 usePost기능 post에 합침 comment추가
    postPart의 vierUsers와 scrapUsers를 post에 속성 추가 하는것으로 수정 firebase에 join기능이 없어서
    
    comment만 남게되어 따로 만듬
    
    usePost 따로 훅으로 있어 유지보수 이슈로 post에 몰아놓음

--- src/firestore/module/post.js
+++ src/firestore/module/post.js
@@ -13,6 +13,11 @@ import {
     startAt,
 } from 'firebase/firestore';
 import { db } from "../firebase";
+import { useQuery } from '@tanstack/react-query';
+import { deleteComment } from './comment';
+import { fsDeleteImage, fsUploadImage } from './image';
+import { writeTime } from '../../components/common/Date';
+
 
 const pageLimit = 3;
 
@@ -22,6 +27,23 @@ export const insertPost = async (post) => {
 
 export const getPost = async (postID) => {
     return await getDoc(doc(db, "post", postID));
+}
+
+export const useGetPost = (postId) => {
+    const getFBPost = async () => {
+        const response = await getPost(postId);
+        return response.data();
+    }
+
+    const result = useQuery(
+        ["getFBPost"],
+        getFBPost,
+    );
+
+    return {
+        post: result.data,
+        postIsLoading: result.isLoading
+    }
 }
 
 export const getPosts = async (searchState, startAfterSnapshot) => {
@@ -51,8 +73,58 @@ export const updatePost = async (postID, post) => {
     return await updateDoc(doc(db, "post", postID), post);
 }
 
+export const onUpdate = (deleteImageName, uploadImage, postId, data) => {
+    if (deleteImageName.length > 0) {
+        deleteImageName.map(async (fileName) => {
+            await fsDeleteImage("images/post", fileName)
+        })
+    }
+
+    if (uploadImage.length > 0) {
+        uploadImage.map(async (file, i) => {
+            const getImageURI = await fsUploadImage("images/post", file, `${localStorage.getItem('uid')}_${file.name}_${writeTime}`);
+            data.photoURIs.push(getImageURI);
+            if (uploadImage.length === (i + 1)) {
+                updatePost(postId, data)
+                    .then(() => {
+                        window.location.reload()
+                    })
+                    .catch(error => {
+                        console.log("post update error", error);
+                    })
+            }
+        })
+    } else {
+        updatePost(postId, data)
+            .then(() => {
+                window.location.reload()
+            })
+            .catch(error => {
+                console.log("post update error", error);
+            })
+    }
+}
+
 export const deletePost = async (postID) => {
     return await deleteDoc(doc(db, "post", postID));
+}
+
+export const onDeletePost = (postId) => {
+    if (window.confirm("게시글을 삭제 하시겠습니까?")) {
+        deletePost(postId)
+            .then(() => {
+                deleteComment(postId)
+                    .then(() => {
+                        window.location.replace("/");
+                    })
+                    .catch(error => {
+                        console.log("deletePostPart error ", error)
+                    })
+            }).catch(error => {
+                console.log("deletePost error ", error);
+            })
+
+    }
 }
 
 export const insertBanner = async (banner) => {
