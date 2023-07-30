f5833e505ed922c451446941a1979fbe1a6e91a1
Minhyeonhong <mhh2029@gmail.com>
2023-07-30 14:52:41 +0900

    Fix : post작성 모듈 추가, 다중 이미지 업로드 수정
    post작성 부분 반복 코드를 줄이기 위해 모듈화함
    
    다중 이미지 업로드시 업로드가 완료되지 않았는데 post작성까지 넘어가는 문제 발생 for문으로 수정했었으나 속도가 너무느려 Promise.all을 이용하여 병렬처리하여 수정함

--- src/firestore/module/post.js
+++ src/firestore/module/post.js
@@ -10,11 +10,10 @@ import {
     limit,
     orderBy,
     startAfter,
-    startAt,
 } from 'firebase/firestore';
 import { db } from "../firebase";
 import { useQuery } from '@tanstack/react-query';
-import { deleteComment } from './comment';
+import { createComment, deleteComment } from './comment';
 import { fsDeleteImage, fsUploadImage } from './image';
 import { writeTime } from '../../components/common/Date';
 
@@ -23,6 +22,36 @@ const pageLimit = 3;
 
 export const insertPost = async (post) => {
     return await addDoc(collection(db, "post"), post);
+}
+
+export const onInsertPost = async (files, obj) => {
+    try {
+        if (files.length > 0) {
+            await Promise.all(
+                files.map(async (file) => {
+                    const getImageURI = await fsUploadImage("images/post", file, `${localStorage.getItem('uid')}_${file.name}_${writeTime}`);
+                    obj.photoURIs.push(getImageURI);
+                })
+            );
+        }
+
+        insertPost(obj)
+            .then(response => {
+                const postID = response._key.path.segments[1];
+                createComment(postID)
+                    .then(() => {
+                        window.location.replace(`/${obj.contentType}/${postID}`);
+                    })
+                    .catch(error => {
+                        console.log("createComment error", error)
+                    })
+            })
+            .catch(error => {
+                console.log("fireStore insert error", error);
+            })
+    } catch (error) {
+        console.log("onInsertPost error", error);
+    }
 }
 
 export const getPost = async (postID) => {
@@ -73,28 +102,25 @@ export const updatePost = async (postID, post) => {
     return await updateDoc(doc(db, "post", postID), post);
 }
 
-export const onUpdate = (deleteImageName, uploadImage, postId, data) => {
-    if (deleteImageName.length > 0) {
-        deleteImageName.map(async (fileName) => {
-            await fsDeleteImage("images/post", fileName)
-        })
-    }
+export const onUpdate = async (deleteImageName, uploadImage, postId, data) => {
+    try {
+        if (deleteImageName.length > 0) {
+            await Promise.all(
+                deleteImageName.map(async (fileName) => {
+                    await fsDeleteImage("images/post", fileName)
+                })
+            );
+        }
 
-    if (uploadImage.length > 0) {
-        uploadImage.map(async (file, i) => {
-            const getImageURI = await fsUploadImage("images/post", file, `${localStorage.getItem('uid')}_${file.name}_${writeTime}`);
-            data.photoURIs.push(getImageURI);
-            if (uploadImage.length === (i + 1)) {
-                updatePost(postId, data)
-                    .then(() => {
-                        window.location.reload()
-                    })
-                    .catch(error => {
-                        console.log("post update error", error);
-                    })
-            }
-        })
-    } else {
+        if (uploadImage.length > 0) {
+            await Promise.all(
+                uploadImage.map(async (file) => {
+                    const getImageURI = await fsUploadImage("images/post", file, `${localStorage.getItem('uid')}_${file.name}_${writeTime}`);
+                    data.photoURIs.push(getImageURI);
+                })
+            );
+        }
+
         updatePost(postId, data)
             .then(() => {
                 window.location.reload()
@@ -102,6 +128,8 @@ export const onUpdate = (deleteImageName, uploadImage, postId, data) => {
             .catch(error => {
                 console.log("post update error", error);
             })
+    } catch (error) {
+        console.log("onUpdate error", error);
     }
 }
 
@@ -109,8 +137,21 @@ export const deletePost = async (postID) => {
     return await deleteDoc(doc(db, "post", postID));
 }
 
-export const onDeletePost = (postId) => {
-    if (window.confirm("게시글을 삭제 하시겠습니까?")) {
+export const onDeletePost = async (postId, photoURIs) => {
+    if (!window.confirm("게시글을 삭제 하시겠습니까?")) return;
+    try {
+        if (photoURIs.length > 0) {
+            await Promise.all(
+                photoURIs.map(async (photoURI) => {
+                    const firstIdx = photoURI.indexOf(localStorage.getItem("uid"));
+                    const lastIdx = photoURI.indexOf("?", firstIdx);
+                    const imgName = decodeURIComponent(photoURI.substring(firstIdx, lastIdx));
+
+                    await fsDeleteImage("images/post", imgName);
+                })
+            );
+        }
+
         deletePost(postId)
             .then(() => {
                 deleteComment(postId)
@@ -124,7 +165,10 @@ export const onDeletePost = (postId) => {
                 console.log("deletePost error ", error);
             })
 
+    } catch (error) {
+        console.log("onDeletePost error", error);
     }
+
 }
 
 export const insertBanner = async (banner) => {
